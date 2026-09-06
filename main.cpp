dd04bc095f11b672be88f87b50fea61b8682eafe
smvax <sharov8055@gmail.com>
2026-09-06 16:50:44 +0300

    Добавлен интерфейс пользователя, реализован подсчет площади треугольника

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
-	return 0;
+    setlocale(LC_ALL, "rus");
+
+    double side = 0.0;
+    double height = 0.0;
+
+    std::cout << "Программа для вычисления площади треугольника." << std::endl;
+    std::cout << "Введите длину стороны треугольника: ";
+    std::cin >> side;
+    std::cout << "Введите высоту, прилегающую к этой стороне: ";
+    std::cin >> height;
+    std::cout << std::endl << "Площадь введенного треугольника равна: " << 0.5 * side * height << std::endl;
+
+    return 0;
 }
